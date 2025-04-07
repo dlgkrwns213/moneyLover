661e2d027a22962bff0034aa4e44cd4978558bf6
yebinnnnn <prettybird1@naver.com>
2025-04-07 11:37:48 +0900

    chore:import font at App.vue

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,19 @@
 <script setup>
-import Navigator from './pages/home/Navigator.vue';
+import Navigator from './pages/home/Navigator.vue'
 </script>
 
 <template>
-  <RouterView/>
-  <Navigator/>
+  <RouterView />
+  <Navigator />
 </template>
 
-<style scoped></style>
+<style scoped>
+@font-face {
+  font-family: 'MyFontBold';
+  src: url('/src/assets/fonts/Cafe24Ssurround-v2.0.ttf') format('truetype');
+}
+@font-face {
+  font-family: 'MyFont';
+  src: url('/src/assets/fonts/Cafe24SsurroundAir-v1.1.ttf') format('truetype');
+}
+</style>
