a450c2de7fc2b507253b45d56bab4e9b740889ce
yebinnnnn <prettybird1@naver.com>
2025-04-07 10:43:10 +0900

    style:initial styling before budget image

--- src/pages/home/BudgetComparison.vue
+++ src/pages/home/BudgetComparison.vue
@@ -1,7 +1,26 @@
+<!-- 담당자: 예빈 -->
+
 <script setup>
+import {reactive, ref, onMounted, computed} from 'vue'
+import axios from 'axios'
+
 </script>
 
 <template>
+    <div class="card mb-3 rounded-4 " style="max-width: 540px;">
+        <div class="row g-0 align-items-center">
+            <div calss="col-md04">
+                <img src="">
+            </div>
+            <div clas="col-md-8">
+                <div class="card-body">
+                    <h5 class="card title mb-2">text</h5>
+                    <p class="card-text text-muted">text</p>
+                    <p class="card-text text-muted">text</p>
+                </div>
+            </div>
+        </div>
+    </div>
 </template>
 
 <style scoped>
