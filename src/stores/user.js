23cd1460e0d250ddda18554f8b0f346ac45abb58
Tarae0419 <tarae0419@gmail.com>
2025-04-08 17:19:13 +0900

    Fix: add userId in pinia

--- src/stores/user.js
+++ src/stores/user.js
@@ -3,12 +3,14 @@ import { defineStore } from 'pinia'
 export const useUserStore = defineStore('user', {
   state: () => ({
     isLoggedIn: false,
+    userId: null,
     token: null,
   }),
   actions: {
-    login(fakeToken) {
+    login(fakeToken, id) {
       this.token = fakeToken
       this.isLoggedIn = true
+      this.userId = id
     },
     logout() {
       this.token = null
